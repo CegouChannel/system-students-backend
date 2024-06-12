4aa42d7edd5664c996ed90126057948d9291eb0c
CegouChannel <104511385+CegouChannel@users.noreply.github.com>
2024-06-12 04:32:22 +0400

    update

--- config/database.js
+++ config/database.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-
 const conn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express"
 import cors from "cors"
 import Router from "./routes/routes.js"
-import 'dotenv'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
@@ -13,3 +15,4 @@ app
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 })
+
